ddb193a99dbf48dd75c3f28ee7160f2d250ab4bb
siberianshield <shieldsiberian@gmail.com>
2026-03-15 18:12:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+let cart = [];
 
+function addToCart(item) {
+  cart.push(item);
+  displayCart();
+}
+
+function displayCart() {
+  const cartList = document.getElementById('cart-items');
+  cartList.innerHTML = '';
+  cart.forEach((item, index) => {
+    const li = document.createElement('li');
+    li.textContent = item;
+    cartList.appendChild(li);
+  });
+}
